9b8f89237aba8582b9503a180da9a05889bf577c
Kathryn <keb97@cornell.edu>
2013-08-21 21:54:25 -0700

    moved the secret token to an application.yml file using figaro gem, didn't run server to try

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WorldTravel::Application.config.secret_token = '3bb73a7d516f59879b439e8c9ca6b83be41b5d032ffe6f81f592a99b64938cf74354628f149036d246b9340fbc7763063c8347d04517f73289f92f6d9ef06b48'
+WorldTravel::Application.config.secret_token = ENV["SECRET_TOKEN"]
